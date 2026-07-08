03105b19b250ece3684387d45778467aed3595cc
HARMANPREEET SINGH BIRDI <harmanbirdi005@gmail.com>
2026-07-08 18:17:47 +0530

    Time: 0 ms (100%), Space: 10.6 MB (50.85%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void getperms(vector<int>&nums, int index, vector<vector<int>>&ans){
+        if(index == nums.size()){
+            ans.push_back({nums});
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            getperms(nums,index+1,ans);
+
+            //backtracking
+            swap(nums[index],nums[i]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        getperms(nums,0,ans);
+        return ans;
+    }
+};
